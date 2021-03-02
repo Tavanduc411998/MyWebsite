ff2753b1513d1923d1aebb2da3d9e2552ca31bb4
Tavanduc411998 <57761776+Tavanduc411998@users.noreply.github.com>
2021-03-02 15:12:44 +0700

    yes

---
+++ js/myscripts.js
@@ -0,0 +1,29 @@
+var theImages = [
+    "../images/20210106_062830.jpg",
+    "../images/20210106_094420.jpg",
+    "../images/149422298_2898000177134451_5495076390718808150_o.jpg",
+    "../images/150723351_2898588917075577_6306578708438786530_o.jpg",
+    "../images/20191005_141852.jpg",
+    "../images/20200129_152359.jpg"
+];
+
+var imageSlider = document.querySelector('#imageSlider');
+var next = document.querySelector("#next");
+var prev = document.querySelector("#prev");
+let index = 0;
+
+next.addEventListener("click", function() {
+    index++;
+    if (index > 5) {
+        index = 5;
+    }
+    imageSlider.setAttribute('src', theImages[index]);
+})
+
+prev.addEventListener("click", function() {
+    index--;
+    if (index < 0) {
+        index = 0;
+    }
+    imageSlider.setAttribute('src', theImages[index]);
+})
